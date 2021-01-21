cc8e7292983afb2246cedf3bce85ab299758249b
znu <znu_1@protonmail.com>
2021-01-21 11:02:14 +0100

    wip: introduced View2D and ContentContainer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { ChakraProvider, Container, extendTheme, Flex } from '@chakra-ui/react';
 
 import { Fonts } from 'fonts/fonts';
 
-import Content from 'components/Content/Content';
+import ContentContainer from 'components/ContentContainer/ContentContainer';
 import Header from 'components/Header/Header';
 import Explanation from 'components/Explanation/Explanation';
 import Footer from 'components/Footer/Footer';
@@ -22,7 +22,7 @@ function App() {
       <Container justifyContent="center" h="100%" maxW="6xl">
         <Flex flexDir="column">
           <Explanation />
-          <Content />
+          <ContentContainer />
           <Footer />
         </Flex>
       </Container>
